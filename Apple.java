8e49fe6673d28d22032654f77afd5a24b41bcec1
ssserious <55954522+ssserious@users.noreply.github.com>
2021-12-13 23:42:36 +0300

    Add files via upload

---
+++ Apple.java
@@ -0,0 +1,11 @@
+public class Apple extends Fruit {
+
+    Apple() {
+        super(1.0f);
+    }
+
+    @Override
+    public String toString(){
+        return "яблоки";
+    }
+}
---
+++ Orange.java
@@ -0,0 +1,11 @@
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+}
